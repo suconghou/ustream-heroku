f5a3ceb6868c0edb62f5f18211d82ab885723a3a
suconghou <1126045770@qq.com>
2020-11-16 23:04:15 +0800

    test

--- go.mod
+++ go.mod
@@ -1,1 +1,10 @@
 module ustream-heroku
+
+go 1.15
+
+require (
+	github.com/suconghou/videoproxy v0.0.0-20201109144010-c7e777dc9644
+	github.com/suconghou/youtubevideoparser v0.0.0-20201027065601-b8d8c8dcd78d // indirect
+	github.com/tidwall/gjson v1.6.3 // indirect
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,103 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	"github.com/suconghou/videoproxy/route"
 )
+
+var (
+	startTime = time.Now()
+	logger    = log.New(os.Stdout, "", 0)
+)
+
+var sysStatus struct {
+	Uptime       string
+	GoVersion    string
+	Hostname     string
+	MemAllocated uint64 // bytes allocated and still in use
+	MemTotal     uint64 // bytes allocated (even if freed)
+	MemSys       uint64 // bytes obtained from system
+	NumGoroutine int
+	CPUNum       int
+	Pid          int
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		logger.Fatal("$PORT must be set")
 	}
 	serve(port)
 }
 
 func serve(port string) error {
+	http.HandleFunc("/status", status)
 	http.HandleFunc("/", routeMatch)
 	return http.ListenAndServe(":"+port, nil)
 }
 
 func routeMatch(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "hello world", http.StatusOK)
+	for _, p := range route.Route {
+		if p.Reg.MatchString(r.URL.Path) {
+			if err := p.Handler(w, r, p.Reg.FindStringSubmatch(r.URL.Path)); err != nil {
+				logger.Print(err)
+			}
+			return
+		}
+	}
+	fallback(w, r)
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	memStat := new(runtime.MemStats)
+	runtime.ReadMemStats(memStat)
+	sysStatus.Uptime = time.Since(startTime).String()
+	sysStatus.NumGoroutine = runtime.NumGoroutine()
+	sysStatus.MemAllocated = memStat.Alloc / 1024  // 当前内存使用量
+	sysStatus.MemTotal = memStat.TotalAlloc / 1024 // 所有被分配的内存
+	sysStatus.MemSys = memStat.Sys / 1024          // 内存占用量
+	sysStatus.CPUNum = runtime.NumCPU()
+	sysStatus.GoVersion = runtime.Version()
+	sysStatus.Hostname, _ = os.Hostname()
+	sysStatus.Pid = os.Getpid()
+	if bs, err := json.Marshal(&sysStatus); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Write(bs)
+	}
+}
+
+func fallback(w http.ResponseWriter, r *http.Request) {
+	const index = "index.html"
+	files := []string{index}
+	if r.URL.Path != "/" {
+		files = []string{r.URL.Path, path.Join(r.URL.Path, index)}
+	}
+	if !tryFiles(files, w, r) {
+		if !tryFiles([]string{index}, w, r) {
+			http.NotFound(w, r)
+		}
+	}
+}
+
+func tryFiles(files []string, w http.ResponseWriter, r *http.Request) bool {
+	for _, file := range files {
+		realpath := filepath.Join("./public", file)
+		if f, err := os.Stat(realpath); err == nil {
+			if f.Mode().IsRegular() {
+				http.ServeFile(w, r, realpath)
+				return true
+			}
+		}
+	}
+	return false
 }
